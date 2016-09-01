74adc86e26a3fff87c38bcd101555fbf4fc0caa4
Emmanuel Madrigal <emma.madri97@gmail.com>
2016-09-01 08:22:02 -0600

    Cambios

--- Temporizer.v
+++ Temporizer.v
@@ -7,14 +7,42 @@ module Temporizer(
     input LeftButton,
     input RightButton,
     input actionButton,
-    output _time,
+    output [15:0] _time,//Contiene el valor del tiempo a mostrar
     output state
     );
 
+reg [1:0] CurrentState = 2'b00;
+/*
+00:Contando
+01:Detenido
+10:Set up
+11:Finalizado
+*/
 
-
-
-
+//Controla los estados cuando se está contando
+always @(posedge clk)
+	if (CurrentState == 0)
+		begin
+		if(_time == 0)
+			begin
+			CurrentState = 3;
+			end
+		end
+		
+//Controla los estados al presionar el boton central
+always @(actionButton)
+	if (CurrentState == 0)
+		begin
+		CurrentState = 1;
+		end
+	else if (CurrentState == 1)
+		begin
+		CurrentState = 0;
+		end
+	else if (CurrentState == 3)
+		begin
+		end
+		
 
 
 endmodule
