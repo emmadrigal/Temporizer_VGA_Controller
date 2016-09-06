eca22a7408c37b153ed3cf883a88b8790a2d64c2
xxMauricioxx <maumonteroj@gmail.com>
2016-09-06 11:21:58 -0600

    Se agrega el for pero tiene error

--- VideoMemorie.v
+++ VideoMemorie.v
@@ -32,6 +32,13 @@ module VideoMemorie(
 reg [9:0] contadorx=10'd0;
 reg [9:0] contadory=10'd0;
 reg [11:0] matriz [0:525][0:800];
+reg [9:0] i;
+reg [9:0] j;
+
+for (i=0;i<526;i=i+1)
+begin
+	
+end
 
 always@(posicionx or posiciony)
 	begin
