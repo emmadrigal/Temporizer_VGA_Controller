6505df3deb76c8b69ed6f65e46b4986661eb2ace
Emmanuel Madrigal <emma.madri97@gmail.com>
2016-09-05 22:23:07 -0600

    Fixed memory connection
    Changed num_drawer for a task, this avoids collition on different numbers

--- temporizer_to_Mem.v
+++ temporizer_to_Mem.v
@@ -5,18 +5,186 @@ module temporizer_to_Mem(
     input [1:0] state,
 	 input clk, //Utilizado para cambiar los colores durante estado "Finalizado"
     output reg [11:0] rgb,
-    output [8:0] x,
-    output [8:0] y
+    output reg [8:0] x,
+    output reg [8:0] y
     );
-	 
-	 
+
 //Variables usadas para la comunicación con drawer
 reg [3:0] num = 0;
 reg [8:0] x0 = 0;
 reg [8:0] y0 = 0;
 reg [11:0] rgb_draw = 0;
+integer i;
+integer j;
 
-num_drawer drawer(.num(num), .x0(x0), .y0(y0), .rgb_in(rgb_draw), .x(x), .y(y), .rgb_out(rgb));
+task draw;
+	input     [3:0]  num; //Numero de 0 a 9
+	begin
+
+		//Pinta de negro todo el recuadro del color para posteriormente pintar de negro el resto
+		for (i = 0; i < 80; i = i + 1) begin
+			 for(j = 0; j < 160; j = j + 1) begin
+				x = x0 + i;
+				y = y0 + i;
+				rgb = rgb_draw;
+			 end
+		end
+
+		//Pinta el negativo en la pantalla, checkea para cada numero y realiza la pintura indicada
+		if (num == 0) begin
+			for (i = 10; i < 70; i = i + 1) begin
+				 for(j = 10; j < 150; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 1) begin
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 0; j < 160; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 2) begin
+				for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 10; j < 75; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+			for (i = 10; i < 80; i = i + 1) begin
+				 for(j = 85; j < 150; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 3) begin
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 10; j < 75; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 85; j < 150; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 4) begin
+			for (i = 10; i < 70; i = i + 1) begin
+				 for(j = 0; j < 75; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 85; j < 160; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 5) begin
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 10; j < 75; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 85; j < 150; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 6) begin
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 0; j < 75; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+			for (i = 10; i < 70; i = i + 1) begin
+				 for(j = 85; j < 150; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 7) begin
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 10; j < 160; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 8) begin
+			for (i = 10; i < 70; i = i + 1) begin
+				 for(j = 10; j < 75; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+			for (i = 10; i < 70; i = i + 1) begin
+				 for(j = 85; j < 150; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+		else if (num == 9) begin
+			for (i = 10; i < 70; i = i + 1) begin
+				 for(j = 10; j < 75; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+			for (i = 0; i < 70; i = i + 1) begin
+				 for(j = 85; j < 160; j = j + 1) begin
+					x = x0 + i;
+					y = y0 + i;
+					rgb = 0;
+				 end
+			end
+		end
+
+	 end
+ endtask
+	 
+
+
 
 reg [15:0] tiempo;
 /*
@@ -49,25 +217,29 @@ begin
 		tiempo = tiempo/600;
 		x0 = 120;
 		y0 = 160;
+		draw(num);
+		
 		end
-	else if (tiempo % 60 != 0)
+	if (tiempo % 60 != 0)
 		begin
 		num = tiempo%60;
 		tiempo = tiempo/60;
 		x0 = 220;
 		y0 = 160;
+		draw(num);
 		end
-	else if (tiempo % 10 != 0)
+	if (tiempo % 10 != 0)
 		begin
 		num = tiempo%10;
 		tiempo = tiempo/600;
 		x0 = 340;
 		y0 = 160;
+		draw(num);
 		end
-	else
-		num = tiempo;
-		x0 = 440;
-		y0 = 160;
+	num = tiempo;
+	x0 = 440;
+	y0 = 160;
+	draw(num);
 end
 
 
