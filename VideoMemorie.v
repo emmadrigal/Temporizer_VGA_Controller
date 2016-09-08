6eecb14f9a80fbb2beeb5651f109530ce2d0c612
xxMauricioxx <maumonteroj@gmail.com>
2016-09-08 08:10:13 -0600

    Creo que ya sirve lo de la ram simulada

--- VideoMemorie.v
+++ VideoMemorie.v
@@ -24,36 +24,50 @@ module VideoMemorie(
 	input VerSin,
 	input posicionx,
 	input posiciony,
-   output Vr,
-   output Vb,
-   output Vg
+   output reg Vr,
+   output reg Vb,
+   output reg Vg
     );
 
 reg [9:0] contadorx=10'd0;
 reg [9:0] contadory=10'd0;
 reg [11:0] matriz [0:525][0:800];
+reg star=1'b0;
 reg [9:0] i;
 reg [9:0] j;
 
-for (i=0;i<526;i=i+1)
-begin
 	
-end
+
 
 always@(posicionx or posiciony)
 	begin
-	matriz[posiciony][posicionx]=Coloresrgb;
+	matriz[posiciony+45][posicionx+160]=Coloresrgb;
 	end 
 
 
 always @(Pxcount)
 	begin
-	contadorx<= contadorx + 1;
+	Vr= matriz[contadory][contadorx][3:0];
+	Vb= matriz[contadory][contadorx][7:4];
+	Vg= matriz[contadory][contadorx][11:8];
+	if(!star)begin
+	star = 1;
+	for(i=0;i<526;i=i+1)
+		for(j=0;j<801;j=j+1)
+			matriz[i][j]=0;
+	end
 	if(contadorx==800)
 		begin
-		contadorx <= 0;
-		contadory <= contadory + 1;
-		end	
+		contadorx = 0;
+		contadory = contadory + 1;
+		end else if(contadory==525)
+			begin
+				contadory = 0;
+				contadorx = 0;
+			end else
+				begin
+				contadorx= contadorx + 1;
+				end
 	end 
 
 endmodule
